c8b4f677677fb7d8eaef7d838744d01400c1947b
Gabe Jensen <thegabejensen@gmail.com>
2017-08-26 09:48:19 -0700

    Show Back To Homework button when done with steps

--- answerharbor_app/static/js/build.js
+++ answerharbor_app/static/js/build.js
@@ -2,7 +2,7 @@
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 require('./post');
 
-}).call(this,require("+7ZJp0"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_fe05ff15.js","/")
+}).call(this,require("+7ZJp0"),typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {},require("buffer").Buffer,arguments[3],arguments[4],arguments[5],arguments[6],"/fake_71361b41.js","/")
 },{"+7ZJp0":6,"./post":2,"buffer":3}],2:[function(require,module,exports){
 (function (process,global,Buffer,__argument0,__argument1,__argument2,__argument3,__filename,__dirname){
 $(document).ready(function() {
