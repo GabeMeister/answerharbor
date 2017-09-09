40ba22b97d16b7b3e676964f603f8339954342ab
Gabe Jensen <thegabejensen@gmail.com>
2017-09-09 12:02:17 -0700

    Fixed edit post question not showing mathjax issue

--- answerharbor_app/static/js/vue/edit_post_app.js
+++ answerharbor_app/static/js/vue/edit_post_app.js
@@ -4,9 +4,9 @@ var app = new Vue({
     el: '#app',
     data: {
         title: '',
-        question: new MathjaxInput('question_input', 'question_buffer', 'question_preview', '', ''),
+        question: new MathjaxInput('question_input', 'question_buffer', 'question_preview', '', '', 1),
         stepGroup: new StepGroup('step'),
-        finalAnswer: new MathjaxInput('final_answer_input', 'final_answer_buffer', 'final_answer_preview', '', ''),
+        finalAnswer: new MathjaxInput('final_answer_input', 'final_answer_buffer', 'final_answer_preview', '', '', 1),
         running: false,
         pending: false,
         timeoutID: 0,
@@ -21,8 +21,6 @@ var app = new Vue({
             .then(response => {
                 this.title = response.data.title;
                 this.question.text = response.data.question;
-                console.log('updating question', this.question);
-                this.update(this.question);
 
                 // TODO: refactor the way /post_data returns the data from the post.
                 // Separate the fake answers from the correct answer so we don't have to
@@ -30,15 +28,11 @@ var app = new Vue({
                 for(var i = 0; i < response.data.answers.length; i++) {
                     if(response.data.answers[i].correct) {
                         this.finalAnswer.text = response.data.answers[i].text;
-                        console.log('updating finalAnswer', this.finalAnswer);
-                        this.update(this.finalAnswer);
                         break;
                     }
                 }
 
                 this.stepGroup.initStepsFromList(response.data.steps);
-
-                this.$validator.updateDictionary(Validator.customErrorMessages());
 
                 this.updateAll();
             })
@@ -71,6 +65,17 @@ var app = new Vue({
                     ["Typeset", MathJax.Hub]
                 );
 
+                // Display question mathjax
+                MathJax.Hub.Queue(
+                    ["displayMathjax", this, this.question]
+                );
+
+                // Display final answer mathjax
+                MathJax.Hub.Queue(
+                    ["displayMathjax", this, this.finalAnswer]
+                );
+
+                // Display mathjax for all steps
                 for(var i = 0; i < this.stepGroup.steps.length; i++) {
                     MathJax.Hub.Queue(
                         ["displayMathjax", this, this.stepGroup.steps[i]]
