fe8d9970c398196aba297d936aaf90193025dfef
Gabe Jensen <thegabejensen@gmail.com>
2017-10-31 05:53:19 -0700

    Submit image url on enter key press

--- answerharbor_app/static/js/vue/components/InputHelpers/ImageButton.js
+++ answerharbor_app/static/js/vue/components/InputHelpers/ImageButton.js
@@ -20,7 +20,8 @@ Vue.component('ImgBtn', {
                                 :id="inputId"
                                 :name="inputId"
                                 v-model="tempImgUrl"
-                                placeholder="Temporary Image Url"/>
+                                placeholder="Temporary Image Url"
+                                @keyup="submitOnEnter"/>
 
                             <div>
                                 <br/>
@@ -43,7 +44,8 @@ Vue.component('ImgBtn', {
                             <button type="button"
                                 class="btn btn-primary"
                                 data-dismiss="modal"
-                                @click="upload">Upload to Imgur</button>
+                                @click="upload"
+                                ref="uploadBtn">Upload to Imgur</button>
                         </div>
                     </div>
 
@@ -103,6 +105,12 @@ Vue.component('ImgBtn', {
             setTimeout(() => {
                 $('#'+this.inputId).get(0).focus();
             }, 500);
+        },
+        submitOnEnter: function(event) {
+            if(event.key === 'Enter') {
+                var uploadBtn = this.$refs.uploadBtn;
+                uploadBtn.click();
+            }
         },
         upload: function() {
             // Check for either the image url or the local file upload
