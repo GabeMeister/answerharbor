c789cc796c2a3036e46491de5b582d873443b477
Gabe Jensen <thegabejensen@gmail.com>
2017-11-07 06:23:49 -0800

    Added multiple chromeless tests

--- tests/main.js
+++ tests/main.js
@@ -1,17 +1,46 @@
 const { Chromeless } = require('chromeless');
 const { expect } = require('chai');
 
-describe('Arriving at website', function() {
-    it('has call to action button', async function() {
+describe('Navigating Website', function() {
+    let chromeless;
+
+    before(function() {
+        chromeless = new Chromeless({
+            debug: true
+        });
+    });
+
+
+    it('Can view posts', async function() {
         this.timeout(10000);
-        const chromeless = new Chromeless();
 
         await chromeless.goto('https://answerharbor.com')
-            .wait('div.body-content');
+            .click('a[href="/select_school"]')
+            .click('a[href="/school/1657"]')
+            .click('a[href="/course/2"]')
+            .click('a[href="/homework/5"]')
+            .wait(400);
 
-        const result = await chromeless.exists('a[href="/select_school"]');
-
+        const result = await chromeless.exists('a[href*="/post/"]');
         expect(result).to.be.true;
+    });
+
+
+    it('Can view schools', async function() {
+        this.timeout(10000);
+
+        await chromeless.goto('https://answerharbor.com')
+            .click('a[href="/select_school"]')
+            // .click('a[href="/school/1657"]')
+            // .click('a[href="/course/2"]')
+            .wait(400);
+
+        const result = await chromeless.exists('a[href*="/school/"]');
+        expect(result).to.be.true;
+    });
+
+
+    after(async function() {
         await chromeless.end();
     });
 });
