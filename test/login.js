2a129ab2d12f44ea2ceb9c5618b27805e25b06bd
Gabe Jensen <thegabejensen@gmail.com>
2017-11-10 09:35:34 -0800

    Switched tests to ava, began login tests

--- test/login.js
+++ test/login.js
@@ -1,29 +1,51 @@
-const { Chromeless } = require('chromeless');
-const { expect } = require('chai');
+import {test} from 'ava';
+import Chromeless from 'chromeless';
+import 'jquery';
 
-describe('Logins', function() {
-    var chromeless;
+test('login works', async t => {
+    const chromeless = new Chromeless({
+        launchChrome: false,
+        // scrollBeforeClick: true,
+        viewport: {
+            width: 1500,
+            height: 900,
+            scale: 1
+        }
+    });
 
-    before(function() {
-        chromeless = new Chromeless({
-            debug: true
+    // TODO: figure out what is going on with the login button
+    let currentlySignedIn = await chromeless
+        .exists('a[href="/logout"]');
+
+    if(currentlySignedIn) {
+        t.log('yup, signed in');
+        await chromeless
+            .click();
+    }
+    else {
+        t.log('not signed in!');
+    }
+
+    await chromeless
+        .goto('http://localhost:5000')
+        .click('a[href="/login"]')
+        .type('fake1', 'input[name="username"')
+        .type('12341234', 'input[name="password"]')
+        .click('input[name="submit"]')
+        .wait(400);
+
+    // Check for the welcome text in the nav bar
+    t.log('yes');
+    const result = await chromeless
+        .evaluate(() => {
+            return $('.navbar-text').text();
         });
-    });
 
-    it('can view homeworks', async function() {
-        this.timeout(10000);
+    await chromeless
+        .click('a[href="/logout"]')
+        .wait(400);
 
-        await chromeless.goto('http://localhost:5000')
-            .click('a[href="/select_school"]')
-            .click('a[href="/school/1657"]')
-            .click('a[href="/course/2"]')
-            .wait(400);
+    await chromeless.end();
 
-        const result = await chromeless.exists('a[href*="/school/"]');
-        expect(result).to.be.true;
-    });
-
-    after(async function() {
-        // await chromeless.end();
-    });
+    t.is(result, 'Welcome, fake1');
 });
--- test/navigation.js
+++ test/navigation.js
@@ -1,30 +1,63 @@
-const { Chromeless } = require('chromeless');
-const { expect } = require('chai');
+import {test} from 'ava';
+import Chromeless from 'chromeless';
 
-describe('Navigating Website', function() {
-    var chromeless;
+test('posts exist', async t => {
+    const chromeless = new Chromeless({ launchChrome: false });
+    await chromeless
+        .goto('http://localhost:5000')
+        .click('a[href="/select_school"]')
+        .click('a[href="/school/1657"]')
+        .click('a[href="/course/2"]')
+        .click('a[href="/homework/5"]')
+        .wait(400);
 
-    before(function() {
-        chromeless = new Chromeless({
-            debug: true
-        });
-    });
+    const result = await chromeless.exists('a[href*="/post/"]');
+    await chromeless.end();
 
-    it('can view posts', async function() {
-        this.timeout(10000);
+    t.true(result);
+});
 
-        await chromeless.goto('http://localhost:5000')
-            .click('a[href="/select_school"]')
-            .click('a[href="/school/1657"]')
-            .click('a[href="/course/2"]')
-            .click('a[href="/homework/5"]')
-            .wait(400);
 
-        const result = await chromeless.exists('a[href*="/post/"]');
-        expect(result).to.be.true;
-    });
+test('homeworks exist', async t => {
+    const chromeless = new Chromeless({ launchChrome: false });
+    await chromeless
+        .goto('http://localhost:5000')
+        .click('a[href="/select_school"]')
+        .click('a[href="/school/1657"]')
+        .click('a[href="/course/2"]')
+        .wait(400);
 
-    after(async function() {
-        // await chromeless.end();
-    });
+    const result = await chromeless.exists('a[href*="/homework/"]');
+    await chromeless.end();
+
+    t.true(result);
+});
+
+
+test('courses exist', async t => {
+    const chromeless = new Chromeless({ launchChrome: false });
+    await chromeless
+        .goto('http://localhost:5000')
+        .click('a[href="/select_school"]')
+        .click('a[href="/school/1657"]')
+        .wait(400);
+
+    const result = await chromeless.exists('a[href*="/course/"]');
+    await chromeless.end();
+
+    t.true(result);
+});
+
+
+test('schools exist', async t => {
+    const chromeless = new Chromeless({ launchChrome: false });
+    await chromeless
+        .goto('http://localhost:5000')
+        .click('a[href="/select_school"]')
+        .wait(400);
+
+    const result = await chromeless.exists('a[href*="/school/"]');
+    await chromeless.end();
+
+    t.true(result);
 });
